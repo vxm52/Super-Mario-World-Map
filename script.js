072d9704bd5dd8e01a5b5d4c04c58a43534841dd
vxm52 <vaibhav.s.malla@gmail.com>
2021-03-16 19:30:00 -0400

    Updated starting coordinates and moved icons to folder

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var token = config.MY_API_TOKEN;
 var my_map_id = config.MY_MAP_ID;
-console.log("My map id: " + my_map_id);
+
+// console.log("My map id: " + my_map_id);
 
 // document.getElementById("maps-script").src= 'https://maps.googleapis.com/maps/api/js?key=' + token + '&map_ids=' + my_map_id + '&callback=initMap';
 // console.log(document.getElementById('maps-script').src);
@@ -9,15 +10,16 @@ function initMap() {
   // Update MAP_ID with custom map ID
   map = new google.maps.Map(document.getElementById("map"), {
     center: {
-      lat: 34.66767774804736,
-      lng: 135.43076145097373,
+      lat: 40.765091,
+      lng: -73.977285,
     },
-    zoom: 18,
+    zoom: 15,
     mapId: my_map_id,
-    mapTypeControl: false,
+    // mapTypeId: "satellite",
     fullscreenControl: false,
     streetViewControl: false,
   });
+
 
 
   // Name
@@ -25,59 +27,18 @@ function initMap() {
   // Image URL
   // scaledSize width, height
   const markers = [
-    [
-      "Yoshi's House",
-      34.66669734177897,
-      135.4309054875526,
-      "yoshi_house.svg",
-      38,
-      31,
-    ],
-    [
-      "You Are Here",
-      34.66767112713121,
-      135.4297887322531,
-      "pointer.svg",
-      30,
-      47.8,
-    ],
-    [
-      "Ghost House",
-      34.66832715150856,
-      135.43292762674864,
-      "ghosthouse.svg",
-      40,
-      48,
-    ],
-    ["Castle", 34.66775608022106, 135.43139547897843, "castle.svg", 40, 53],
-    ["Warp Pipe", 34.66739738878135, 135.43225049775214, "pipe.svg", 38, 42.5],
-    ["Star World", 34.667959023359266, 135.42866400953733, "star.svg", 38, 38],
-    [
-      "Donut Plains",
-      34.66830355359945,
-      135.4320565322381,
-      "hill_with_eyes.svg",
-      50,
-      60.7,
-    ],
-    [
-      "Donut Plains",
-      34.66829411443392,
-      135.43231361996433,
-      "hill_with_eyes.svg",
-      50,
-      60.7,
-    ],
-    [
-      "Donut Plains",
-      34.6683781779677,
-      135.43217016043528,
-      "hill_with_eyes.svg",
-      50,
-      60.7,
-    ],
+    ["Yoshi's House", 40.765091, -73.977285, "icons/yoshi_house.svg", 38, 31],
+    ["You Are Here", 40.761091, -73.979285, "icons/pointer.svg", 30, 47.8],
+    ["Ghost House", 40.758991, -73.974285, "icons/ghosthouse.svg", 40, 48],
+    ["Castle", 40.773091, -73.968185, "icons/castle.svg", 40, 53],
+    ["Warp Pipe", 40.766691, -73.989285, "icons/pipe.svg", 38, 42.5],
+    ["Star World", 40.764091, -73.965285, "icons/star.svg", 38, 38],
+    ["Donut Plains", 40.768991, -73.977285, "icons/hill_with_eyes.svg", 50, 60.7],
+    ["Donut Plains", 40.76841, -73.977985, "icons/hill_with_eyes.svg", 50, 60.7],
+    ["Donut Plains", 40.76821, -73.976485, "icons/hill_with_eyes.svg", 50, 60.7],
   ];
 
+  // Create and animate the markers
   for (let i = 0; i < markers.length; i++) {
     const currMarker = markers[i];
 
